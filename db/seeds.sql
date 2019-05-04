0970fd9cc7d869860fc775066687c5fdbb3a201b
BrielleCarvalho <brielle.127@gmail.com>
2019-05-04 11:34:39 -0500

    adding burgers

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers
+VALUES(NULL, "Bacon and Blue Cheese Burger", false);
+
+INSERT INTO burgers
+VALUES(NULL, "Ahi Tuna Burger", false);
+
+INSERT INTO burgers
+VALUES(NULL, "Spiced Lamb Burger", false);
